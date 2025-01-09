ef57abfd06f652d0c5ae9af219dbadc949c3b20b
suinmoon <suinmoon83@gmail.com>
2025-01-09 19:49:05 +0900

    250109
    || 핵심 내용 ||
    - VideoPlayer와 Clip연결
    - 클래스 변환
    - 파일 경로로 불러오기
    - VIDEOPLAYER privite으로 변경

---
+++ Assets/03_Script/Common/Shared.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+public static class Shared
+{
+    public static SceneMgr SceneMgr;
+    //public static BattleMgr BattleMgr;
+
+}
--- Assets/03_Script/SceneManager/SceneMgr_Atlas.cs
+++ Assets/03_Script/SceneManager/SceneMgr_Atlas.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.U2D;
 
 public partial class SceneMgr : MonoBehaviour
@@ -10,9 +11,16 @@ public partial class SceneMgr : MonoBehaviour
     [NonReorderable]
     Dictionary<string,SpriteAtlas> DicSpriteAtlas = new Dictionary<string, SpriteAtlas> ();
 
+    public Image atlas;
+    public string spriteName;
+    
+    //public Sprite atlasSprite;
+    //public int atlasNumber;
+
     private void Update()
     {
-        GetSpriteAtlas("Number_UI02", "2");
+        atlas = GetComponent<Image>();
+        atlas.sprite = GetSpriteAtlas("Number_UI01", $"{spriteName}");
     }
 
     public Sprite GetSpriteAtlas(string _Atlas, string _Name)
